0b1b3bc9b223f13a7dd9cbc1daae729862b11ae4
DA-SILVA-FERNANDES Diogo <Diogo.DA-SILVA-FERNANDES@cpnv.ch>
2021-03-01 14:20:11 +0100

    Implémentation du menu du jeu plus accès au sous-menus non-finis

--- main.c
+++ main.c
@@ -6,93 +6,127 @@ int main() {
 
     // Declaration de variables
     char colChoice;
-    int lineChoice = 0, colChoiceNb = 0, gameOn = 1;
+    int lineChoice = 0, colChoiceNb = 0, gameOn = 1, menuChoice = 0;
+    int nbrPorteAvion = 1, nbrCroiseur = 1, nbrContreTorpilleur = 2, nbrTorpilleur = 1;
 
     // Declaration du tableau de jeu
     char gameBoard[11][11] = {
-            {' ','1','2','3','4','5','6','7','8','9','0'},
-            {'A',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-            {'B',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-            {'C',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-            {'D',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-            {'E',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-            {'F',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-            {'G',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-            {'H',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-            {'I',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-            {'J',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '}
+            {' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'},
+            {'A', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'B', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'C', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'D', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'E', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'F', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'G', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'J', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
     };
 
-    printf("Lancement de la partie ! Bonne chance !\n\n");
+    printf("Bienvenue !\n");
 
-    // Afficher le tableau
-    printf("\n        ╔═══════╦═══════╦═══════╦═══════╦═══════╦═══════╦═══════╦═══════╦═══════╦═══════╦═══════╗\n");
-    printf("        ║       ║       ║       ║       ║       ║       ║       ║       ║       ║       ║       ║\n");
+    while (gameOn == 1) {
 
-    for (int col = 0; col < 11; ++col) {
-        printf("        ║");
-
-        for (int line = 0; line < 11; ++line) {
-            if (col == 10 && line == 0){
-                printf("  10   ║");
-            }else{
-                printf("   %c   ║",gameBoard[line][col]);
-            }
+        while (menuChoice < 1 || menuChoice > 3){
+            printf("\nMENU DU JEU\n"
+                   "    1 - Jouer\n"
+                   "    2 - Aide de Jeu\n"
+                   "    3 - Quitter\n\n"
+                   "Que voulez-vous faire :"
+            );
+            scanf("%d",&menuChoice);
         }
 
-        printf("\n        ║       ║       ║       ║       ║       ║       ║       ║       ║       ║       ║       ║\n");
+        switch (menuChoice) {
+            case 1:
+                system("cls");
+                printf("Lancement de la partie ! Bonne chance !\n\n");
 
-        if (col != 10){
-            printf("        ╠═══════╬═══════╬═══════╬═══════╬═══════╬═══════╬═══════╬═══════╬═══════╬═══════╬═══════╣\n");
-            printf("        ║       ║       ║       ║       ║       ║       ║       ║       ║       ║       ║       ║\n");
+                do {
+                    // Afficher le tableau
+                    printf("\n        ╔═══════╦═══════╦═══════╦═══════╦═══════╦═══════╦═══════╦═══════╦═══════╦═══════╦═══════╗\n");
+                    printf("        ║       ║       ║       ║       ║       ║       ║       ║       ║       ║       ║       ║\n");
 
-        }else{
-            printf("        ╚═══════╩═══════╩═══════╩═══════╩═══════╩═══════╩═══════╩═══════╩═══════╩═══════╩═══════╝\n");
+                    for (int col = 0; col < 11; ++col) {
+                        printf("        ║");
+
+                        for (int line = 0; line < 11; ++line) {
+                            if (col == 10 && line == 0) {
+                                printf("  10   ║");
+                            } else {
+                                printf("   %c   ║", gameBoard[line][col]);
+                            }
+                        }
+
+                        printf("\n        ║       ║       ║       ║       ║       ║       ║       ║       ║       ║       ║       ║\n");
+
+                        if (col != 10) {
+                            printf("        ╠═══════╬═══════╬═══════╬═══════╬═══════╬═══════╬═══════╬═══════╬═══════╬═══════╬═══════╣\n");
+                            printf("        ║       ║       ║       ║       ║       ║       ║       ║       ║       ║       ║       ║\n");
+
+                        } else {
+                            printf("        ╚═══════╩═══════╩═══════╩═══════╩═══════╩═══════╩═══════╩═══════╩═══════╩═══════╩═══════╝\n");
+                        }
+                    }
+
+                    //Vérification que la colone choisie est entre A et J
+                    do {
+                        printf("\n\n-> Colonne (A à J -> En majuscule):");
+                        scanf("%s", &colChoice);
+
+                        if (colChoice < 65 || colChoice > 74) {
+                            printf("Vous n'avez pas choisi une lettre en A et J !\n");
+                        }
+                    } while (colChoice < 65 || colChoice > 74);
+
+                    //Vérification que la ligne choisie est entre 1 et 10
+                    do {
+                        printf("-> Ligne (1 à 10):");
+                        scanf("%d", &lineChoice);
+
+                        if (lineChoice < 1 || lineChoice > 10) {
+                            printf("Vous devez choisir que des chiffres entre 1 et 10 !\n");
+                        }
+                    } while (lineChoice < 1 || lineChoice > 10);
+
+                    printf("%d", colChoice);
+
+                    // Transforme lettre en numéro entre 1 et 10
+                    if (colChoice == 65) {
+                        colChoiceNb = 1;
+                    } else if (colChoice == 66) {
+                        colChoiceNb = 2;
+                    } else if (colChoice == 67) {
+                        colChoiceNb = 3;
+                    } else if (colChoice == 68) {
+                        colChoiceNb = 4;
+                    } else if (colChoice == 69) {
+                        colChoiceNb = 5;
+                    } else if (colChoice == 70) {
+                        colChoiceNb = 6;
+                    } else if (colChoice == 71) {
+                        colChoiceNb = 7;
+                    } else if (colChoice == 72) {
+                        colChoiceNb = 8;
+                    } else if (colChoice == 73) {
+                        colChoiceNb = 9;
+                    } else if (colChoice == 74) {
+                        colChoiceNb = 10;
+                    }
+
+                    system("cls");
+                } while (nbrPorteAvion > 1 || nbrCroiseur > 1 || nbrContreTorpilleur > 2 || nbrTorpilleur > 1);
+                break;
+            case 2:
+                printf("\nEN DEVELOPEMENT !\n");
+                break;
+            case 3:
+                gameOn = 2;
+                break;
+            default:
+                break;
         }
     }
-
-    //Vérification que la colone choisie est entre A et J
-    do{
-        printf("\n\n-> Colonne (A à J -> En majuscule):");
-        scanf("%s", &colChoice);
-
-        if (colChoice < 65 || colChoice > 74){
-            printf("Vous n'avez pas choisi une lettre en A et J !\n");
-        }
-    }while (colChoice < 65 || colChoice > 74);
-
-    //Vérification que la ligne choisie est entre 1 et 10
-    do{
-        printf("-> Ligne (1 à 10):");
-        scanf("%d", &lineChoice);
-
-        if (lineChoice < 1 || lineChoice > 10){
-            printf("Vous devez choisir que des chiffres entre 1 et 10 !\n");
-        }
-    }while (lineChoice < 1 || lineChoice > 10);
-
-    // Transforme lettre en numéro entre 1 et 10
-    if (colChoice == 65) {
-        colChoiceNb = 1;
-    } else if (colChoice == 66) {
-        colChoiceNb = 2;
-    } else if (colChoice == 67) {
-        colChoiceNb = 3;
-    } else if (colChoice == 68) {
-        colChoiceNb = 4;
-    } else if (colChoice == 69) {
-        colChoiceNb = 5;
-    } else if (colChoice == 70) {
-        colChoiceNb = 6;
-    } else if (colChoice == 71) {
-        colChoiceNb = 7;
-    } else if (colChoice == 72) {
-        colChoiceNb = 8;
-    } else if (colChoice == 73) {
-        colChoiceNb = 9;
-    } else if (colChoice == 74) {
-        colChoiceNb = 10;
-    }
-
-    return 0;
+        return 0;
 }
